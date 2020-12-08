5765b23f060628d40c3c8136296c4cf8e29f06d9
Meerfolk <meerfolk@gmail.com>
2020-12-08 22:37:11 +0300

    Add ConnectionMenu component

---
+++ packages/ui/src/components/ConnectionList/ConnectionMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-dropdown size="sm" variant="link">
+    <template #button-content>
+      <b-icon icon="gear" size="20" />
+    </template>
+    <b-dropdown-item @click="remove()" variant="danger">Delete</b-dropdown-item>
+  </b-dropdown>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { BIcon, BDropdown, BDropdownItem } from 'bootstrap-vue';
+
+export default Vue.extend({
+  name: 'connection-menu',
+  components: {
+    'b-icon': BIcon,
+    'b-dropdown': BDropdown,
+    'b-dropdown-item': BDropdownItem,
+  },
+  props: {
+    connectionId: String,
+  },
+  methods: {
+    remove() {
+      this.$store.dispatch('removeConnection', this.$props.connectionId);
+    },
+  },
+});
+</script>
